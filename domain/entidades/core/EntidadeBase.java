5b081099e51c05635ffadff5e6d6dd80e34ba59d
VictorGMB <victormedeiros2580@hotmail.com>
2026-05-07 11:06:10 -0300

    feat: atualiza EntidadeBase

--- domain/entidades/core/EntidadeBase.java
+++ domain/entidades/core/EntidadeBase.java
@@ -1,11 +1,27 @@
 package domain.entidades.core;
 
+import java.util.EnumMap;
+import java.util.Map;
+
+import domain.entidades.core.enums.TipoDefesa;
+import domain.entidades.core.interfaces.Habilidade;
+import domain.entidades.core.valueobjects.Defesa;
+import domain.entidades.core.valueobjects.Descricao;
 import domain.entidades.core.valueobjects.Nome;
 import domain.entidades.core.valueobjects.Vida;
 
 public class EntidadeBase {
 
-    Nome nome;
-    Vida vida;
+    private Nome nome;
+    private Descricao descricao;
+    private Vida vida;
+    private final Map<TipoDefesa, Defesa> defesas;
+    private final Habilidade[] slots = new Habilidade[4];
+
+    public EntidadeBase(String nome, String descricao, int vida) {
+        this.nome = new Nome(nome);
+        this.vida = new Vida(vida);
+        this.defesas = new EnumMap<>(TipoDefesa.class);
+    }
 
 }
